05f35048576dd70c81f4e8eeb02324fe9ba4b619
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-02 02:01:16 +0530

    * Index.html node_modules package.json postcss.config.js public push.sh README.md server.js src tailwind.config.js tsconfig.json tsconfig.node.json vite.config.ts yarn.lock Third push .

--- src/components/DataGrid/MovieComponent.tsx
+++ src/components/DataGrid/MovieComponent.tsx
@@ -38,7 +38,7 @@ const MovieComponent: React.FC<MovieProps> = ({ title, LockKey, img }) => {
   return (
     <div
       tabIndex={0}
-      className="bg-white rounded-lg shadow-md p-4 mb-4 overflow-hidden"
+      className="bg-white rounded-lg shadow-md p-4 mb-4 overflow-hidden cursor-pointer"
       style={{ width: "200px", height: "400px" }}
     >
       <img
@@ -50,7 +50,7 @@ const MovieComponent: React.FC<MovieProps> = ({ title, LockKey, img }) => {
         <h3 className="text-lg font-semibold">{title}</h3>
         <button
           onClick={handleClick}
-          className="text-white bg-blue-400 hover:bg-blue-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+          className="text-white bg-blue-400 cursor-pointer hover:bg-blue-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
           tabIndex={1}
         >
           Play Now
@@ -69,7 +69,6 @@ const MovieComponent: React.FC<MovieProps> = ({ title, LockKey, img }) => {
           )}
         </Modal>
       )}
-
     </div>
   );
 };
